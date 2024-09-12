9e0b952804b389b0c564d27b8b1f931221435dc6
ggvictor <victorrodriguessilva2@gmail.com>
2024-09-12 18:49:18 -0300

    finalizado

--- results-summary-component-main/js/scripts.js
+++ results-summary-component-main/js/scripts.js
@@ -7,28 +7,61 @@ const reacaoNumerber = document.querySelector("#reacao-number");
 const memoriaNumerber = document.querySelector("#memoria-number");
 const verbalNumerber = document.querySelector("#verbal-number");
 const visualNumerber = document.querySelector("#visual-number");
+const porcentagem = document.querySelector("#porcentagem");
+const number = document.querySelector("#number")
+const text = document.querySelector("#text") 
 
-const resultados ={
-
-    reacao: Math.floor(Math.random()*100) +1,
-    memoria: Math.floor(Math.random()*100) +1,
-    verbal: Math.floor(Math.random()*100) +1,
-    visual: Math.floor(Math.random()*100) +1,
-
-};
-const reacao = resultados.reacao;
-const memoria = resultados.memoria;
-const verbal = resultados.verbal;
-const visual = resultados.visual;
 
 // funções
 
 function mudarResult() {
+
+   const reacao= Math.floor(Math.random()*100) +1;
+   const memoria= Math.floor(Math.random()*100) +1;
+   const verbal= Math.floor(Math.random()*100) +1;
+   const visual= Math.floor(Math.random()*100) +1;
+
+
     reacaoNumerber.innerHTML = reacao;
     memoriaNumerber.innerHTML = memoria;
     verbalNumerber.innerHTML = verbal;
     visualNumerber.innerHTML = visual;
+
+
+    // porcentagem
+    const total = 400;
+    const soma = reacao + memoria + verbal + visual;
+    const porcentagemCalc = (soma /total) * 100; 
+    porcentagem.innerHTML = `${porcentagemCalc.toFixed(1)}%`;
+    const numberValue = porcentagemCalc.toFixed(0);
+    number.innerHTML = `${numberValue}`
+    console.log(soma)
+
+    // condições
+
+    if (numberValue >= 1 && numberValue <= 10) {
+        text.innerHTML = "Péssimo";
+    } else if (numberValue >= 11 && numberValue <= 20) {
+        text.innerHTML = "Muito ruim";
+    } else if (numberValue >= 21 && numberValue <= 30) {
+        text.innerHTML = "Ruim";
+    } else if (numberValue >= 31 && numberValue <= 40) {
+        text.innerHTML = "Quase lá";
+    } else if (numberValue >= 41 && numberValue <= 50) {
+        text.innerHTML = "Ok";
+    } else if (numberValue >= 51 && numberValue <= 60) {
+        text.innerHTML = "Medíocre";
+    } else if (numberValue >= 61 && numberValue <= 70) {
+        text.innerHTML = "Bom";
+    } else if (numberValue >= 71 && numberValue <= 80) {
+        text.innerHTML = "Muito bom";
+    } else if (numberValue >= 81 && numberValue <= 90) {
+        text.innerHTML = "Ótimo";
+    } else if (numberValue >= 91 && numberValue <= 100) {
+        text.innerHTML = "Incrível";
+    }
 };
+
 
 
 // eventos
@@ -36,6 +69,7 @@ buttonDraw.addEventListener("click",()=>{
     draw.style.display = "none";
     result.style.display = "flex";
     mudarResult();
+    
 });
 buttonResult.addEventListener("click",()=>{
     draw.style.display = "flex";
